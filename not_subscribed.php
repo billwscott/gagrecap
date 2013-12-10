2ea96c4715a61d00ec1b875cb2d93f8cb8dc6655
Bill Scott <bilscott@paypal.com>
2013-12-09 22:02:42 -0800

    add this to the repo

---
+++ not_subscribed.php
@@ -0,0 +1,91 @@
+ <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Van B. Scott, III" content="">
+    <link rel="shortcut icon" href="favicon.png">
+
+    <title>The New Gag Recap: Cancel</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/gagrecap.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript" src="//use.typekit.net/spv8ykt.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  </head>
+
+  <body>
+<p>
+ <?php
+/*
+if ($_POST) {
+  $kv = array();
+  foreach ($_POST as $key => $value) {
+    $kv[] = "$key=$value";
+  }
+  $query_string = join("&", $kv);
+}
+else {
+  $query_string = $_SERVER['QUERY_STRING'];
+}
+echo $query_string;
+*/
+?>
+</p>
+
+      <h1><span id="theNew">The New &nbsp;</span><span id="gagRecap">Gag Recap</span>
+      <h2>The gag writers &amp; cartoonists trade journal for the 21st century</h2>
+
+    <div class="container">
+
+      <!-- Static navbar -->
+      <div class="navbar navbar-default">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Gag Recap</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="sample.html">Sample</a></li>
+            <li><a href="cancel.html">Cancel</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+         </div><!-- /.navbar-collapse -->
+      </div>
+
+
+      <div class="jumbotron">
+        <p>
+           You decided to not subscribe. If there are any questions I can answer please <a href="mailto:van.scott@gmail.com">let me know</a>.
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>  </body>
+</html>
+
+<?php
+?>
---
+++ subscribed.php
@@ -0,0 +1,95 @@
+ <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Van B. Scott, III" content="">
+    <link rel="shortcut icon" href="favicon.png">
+
+    <title>The New Gag Recap: Subscribe Successful</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/gagrecap.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript" src="//use.typekit.net/spv8ykt.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  </head>
+
+  <body>
+<p>
+ <?php
+/*
+if ($_POST) {
+  $kv = array();
+  foreach ($_POST as $key => $value) {
+    $kv[] = "$key=$value";
+  }
+  $query_string = join("&", $kv);
+}
+else {
+  $query_string = $_SERVER['QUERY_STRING'];
+}
+echo $query_string;
+*/
+?>
+
+</p>
+
+      <h1><span id="theNew">The New &nbsp;</span><span id="gagRecap">Gag Recap</span>
+      <h2>The gag writers &amp; cartoonists trade journal for the 21st century</h2>
+
+    <div class="container">
+
+      <!-- Static navbar -->
+      <div class="navbar navbar-default">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Gag Recap</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="sample.html">Sample</a></li>
+            <li><a href="cancel.html">Cancel</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+         </div><!-- /.navbar-collapse -->
+      </div>
+
+
+      <div class="jumbotron">
+        <p>
+          Thank you for subscribing. You will be added to the mailing list and receive the current issue in the next 48 hours. 
+          We will use the email address you provided during the subscription process with PayPal. If you want to use a different
+          email address for the newsletter, please <a href="mailto:van.scott@gmail.com?subject=Use%20this%20email%20instead&body=PROVIDE YOUR PREFERRED EMAIL">email me</a> 
+          and provide your preferred email address.
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>  </body>
+</html>
+
+<?php
+?>
